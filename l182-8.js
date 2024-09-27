c9a1eb94f74b70a31c81df17485c397c8c238c65
Yuto Hasegawa <yy6056anderson@outlook.com>
2024-09-27 10:18:33 +0800

    Create l182-8.js

---
+++ l182-8.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+ function sayHello (name, message) {
+ document.write(name + ": " + message);
+ }
+</script>
+
+---------------------------
+
+<script type="text/javascript">
+ sayHello("Peter", "JavaScript is easy!");
+</script>
