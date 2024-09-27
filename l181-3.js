cea73d17cb5f1794ab6cf01ed7571c618eb20595
Yuto Hasegawa <yy6056anderson@outlook.com>
2024-09-27 10:23:16 +0800

    Create l181-3.js

---
+++ l181-3.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+ function test(){
+ document.write("Browser detail: " + navigator.userAgent);
+ document.write("<br>");
+ document.write("Language: " + navigator.language);
+ }
+</script>
+
+-----------------------------
+
+<body onload="test()">
