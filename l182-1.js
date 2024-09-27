bc3d5cb2c0655830478cb3560aa16ef8069ca868
Yuto Hasegawa <yy6056anderson@outlook.com>
2024-09-27 10:22:47 +0800

    Create l182-1.js

---
+++ l182-1.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+ var a = 8;
+ var b = 5;
+ document.write(a + b);
+ document.write("<br>");
+ document.write(a - b);
+ document.write("<br>");
+ document.write(a * b);
+ document.write("<br>");
+ document.write(a / b);
+ document.write("<br>");
+ document.write(a % b);
+</script>
