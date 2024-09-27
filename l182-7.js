e334bf2938c68d9a1d368a08a9aeb06402ea70de
Yuto Hasegawa <yy6056anderson@outlook.com>
2024-09-27 10:19:21 +0800

    Create l182-7.js

---
+++ l182-7.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+ var meal="B";
+ switch (meal){
+ case "A":
+ document.write("Meal A");
+ break;
+ case "B":
+ document.write("Meal B");
+ break;
+ case "C":
+ document.write("Meal C");
+ break;
+ default: 
+document.write("We do not provide it."); 
+}
+</script>
